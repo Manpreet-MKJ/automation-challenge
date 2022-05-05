7250ff89542c7c7ea2a5d51939f1562522b41e04
Manpreet Kaur <manpreet.kaur@amcsgroup.com>
2022-05-05 08:13:29 -0400

    code changed

--- cypress/integration/section-1.spec.js
+++ cypress/integration/section-1.spec.js
@@ -1,10 +1,16 @@
-const { Section1 } = require('../objects/section-1')
+describe('This is my first test suite', function()
+{
 
-describe('Problem 1', () => {
-  /**
-   * Example:
-   * To access assertSampleApiResponse() from Section1, you can do: Section1.actions.assertSampleApiResponse();
-   *
-   * Test away!
-   */
+it('This is my first test case', function(){
+
+
+cy.visit("https://www.walmart.ca/en");
+cy.get('.e1xoeh2i1').type('cofee maker blackdecker')
+cy.get('.css-1lxthnz').click()
+cy.get('.css-1fnwt3w').should('be.visible')
+cy.wait(5000)
+cy.get('.css-459x84').find('.css-1fnwt3w').contains('Add to cart').click()
+
+} )
 })
+
